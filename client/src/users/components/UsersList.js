d50510d43f4bd4a5c3f1e290e8a5b487ab3b3187
Quas <aaron.hickman201@gmail.com>
2023-07-21 22:22:11 +0100

    update

--- client/src/users/components/UsersList.js
+++ client/src/users/components/UsersList.js
@@ -1,15 +1,25 @@
 import React from 'react'
-// import '/UserList.css';
+import './UsersList.css';
 import UserItem from "./UserItem"
 
 
 const UsersList = (props) => {
-    // if(props.items.length === 0) {
+    if(props.items.length === 0) {
         return (
             <div className="centre">
                 <h2>No users found</h2>
             </div>
         );
     };
-// };
+    return <ul className='users-list'>
+        {props.items.map(user => {
+            return <UserItem 
+            key={user.id} 
+            id={user.id} 
+            image={user.image} 
+            name={user.name} 
+            placeCount={user.places}/>
+        })}
+    </ul>
+};
 export default UsersList
